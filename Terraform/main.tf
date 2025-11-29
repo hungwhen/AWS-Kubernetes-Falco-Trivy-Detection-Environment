780b3e9eb79fbd97800b00f0dd09465635e642f8
Hung Nguyen <hungwhen@gmail.com>
2025-11-28 21:34:14 -0600

    add private subnet

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -70,3 +70,63 @@ resource "aws_subnet" "public-1b" {
 		Name = "public-1b"
 	}
 }
+
+resource "aws_eip" "nat_eip" {
+	domain = "vpc"
+	tags = {
+		Name = "nat-eip"
+	}
+}
+
+resource "aws_nat_gateway" "nat" {
+	allocation_id = aws_eip.nat_eip.id
+	subnet_id = aws_subnet.public-1a.id
+	tags = {
+		Name = "main-natgw"
+	}
+
+	depends_on = [aws_internet_gateway.igw]
+}
+
+resource "aws_route_table" "private_route_table" {
+	
+	vpc_id = aws_vpc.default.id
+
+	route {
+		cidr_block = "0.0.0.0/0"
+		nat_gateway_id  = aws_nat_gateway.nat.id
+	}
+	tags = {
+		Name = "private_route_table"
+	}
+}
+
+resource "aws_route_table_association" "private_1a" {
+	subnet_id = aws_subnet.private-1a.id
+	route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_route_table_association" "private_1b" {
+	subnet_id = aws_subnet.private-1b.id
+	route_table_id = aws_route_table.private_route_table.id
+}
+
+resource "aws_subnet" "private-1a" {
+	vpc_id = aws_vpc.default.id 
+	cidr_block = "10.0.3.0/24"
+	availability_zone = "us-east-1a"
+	map_public_ip_on_launch = false
+	tags = {
+		Name = "private-1a"
+	}
+}
+
+resource "aws_subnet" "private-1b" {
+	vpc_id = aws_vpc.default.id
+	cidr_block = "10.0.4.0/24"
+	availability_zone = "us-east-1a"
+	map_public_ip_on_launch = false
+	tags = {
+		Name = "private-1b"
+	}
+}
